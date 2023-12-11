2fb29756654aa10928a865163145424dcf33569a
dmitrypilipenko91 <dmitrypilipenko91@gmail.com>
2023-12-11 05:19:58 +0300

    First commit of react studying

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyInput from './components/UI/button/input/MyInput';
+import MyButton from './components/UI/button/MyButton';
+import './styles/App.css';
 
 function App() {
+  
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ])
+
+  const [post, setPost] = useState({body: '', title: ''});
+  
+  const addNewPost = (event) => {
+    event.preventDefault();
+    setPosts([...posts, {...post, id: Date.now()}]);
+    setPost({body: '', title: ''})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {/* Управляемый компонент (инпут) */}
+        <MyInput type='text' 
+        placeholder='Название статьи' 
+        value={post.title}
+        onChange={event => setPost({...post, title: event.target.value})}>
+        </MyInput>
+        <MyInput type='text' 
+        placeholder='Описание статьи'
+        value={post.body}
+        onChange={event => setPost({...post, body: event.target.value})}>
+        </MyInput>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Посты про JS'/>
     </div>
   );
 }
