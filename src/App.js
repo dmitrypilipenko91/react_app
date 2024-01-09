2fea05e44a2263833f91898ffa8ef20ebe971627
dmitrypilipenko91 <dmitrypilipenko91@gmail.com>
2024-01-09 07:08:27 +0300

    Implemented pagination

--- src/App.js
+++ src/App.js
@@ -6,24 +6,31 @@ import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyLoader from './components/UI/loader/MyLoader';
 import MyModal from './components/UI/modal/MyModal';
+import Pagination from './components/UI/pagination/Pagination';
 import { useFetching } from './hooks/useFetching';
 import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
+import { getPageCount } from './utils/pages';
 
 function App() {
   
   const [posts, setPosts] = useState([ ]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+  const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   });
   
   useEffect(() => {
-    fetchPosts();
-  }, [ ]);
+    fetchPosts(limit, page);
+  }, []);
   
   const createPost = (newPost) => {
     setPosts ([...posts, newPost]);
@@ -33,6 +40,11 @@ function App() {
   // Получаем post из дочернего элемента
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts(limit, page)
   }
 
   return (
@@ -58,6 +70,11 @@ function App() {
           </div>
         : <PostList posts={sortedAndSearchedPosts} remove={removePost} title='Посты про JS' />
       }
+      <Pagination 
+        page={page} 
+        changePage={changePage} 
+        totalPages={totalPages} 
+      />
     </div>
   );
 }
